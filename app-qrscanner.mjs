b41c9c0e09cb12f3aef8d22efeb9d69cbb0ba84d
Ryoma-03 <tku923336@stu.tamagawa.ac.jp>
2025-08-20 18:14:41 +0900

    third commit23

--- app-qrscanner.mjs
+++ app-qrscanner.mjs
@@ -4,6 +4,6 @@ const i2cAccess = await requestI2CAccess();
 const i2cPort = i2cAccess.ports.get(1);
 const qrscanner = new QRScanner(i2cPort, 0x21);
 await qrscanner.init();
-await qrscanner.setTriggerMode(true);
+await qrscanner.setTriggerMode(0);
 let data = await qrscanner.scanData();
 console.dir(data);
