fbc8a1e8d68db47cd3c273d17e3a2f4d3842181a
Ryoma-03 <tku923336@stu.tamagawa.ac.jp>
2025-08-26 17:20:20 +0900

    アドレス修正

--- app-alarmlightsensor.mjs
+++ app-alarmlightsensor.mjs
@@ -3,7 +3,7 @@ import AlarmLightSensorightsensor from "@chirimen/AlarmLightSensor.js";
 import AlarmLightSensor from "./AlarmLightSensor";
 const i2cAccess = await requestI2CAccess();
 const i2cPort = i2cAccess.ports.get(1);
-const alarmlightsensor = new AlarmLightSensor(i2cPort, 9600);
+const alarmlightsensor = new AlarmLightSensor(i2cPort, 0x13);
 await alarmlightsensor.init();
 for (;;) {
   let data = await alarmlightsensor.readProximity();
